3dc8e713b8d8678f4a47490092dcfa712b47727f
Luis Angel <20221063@itla.edu.do>
2025-05-08 12:02:25 -0400

    Primer Commit

---
+++ PatronPrototype/Persona.cs
@@ -0,0 +1,16 @@
+﻿
+namespace PatronPrototype // Define el espacio de nombres del código
+{
+    // Clase Persona implementa el patrón Prototype con una copia superficial (Shallow Copy)
+    public class Persona : ICloneable
+    {
+        public string nombleCompleto { get; set; } // Propiedad para almacenar el nombre
+        public int edad { get; set; } // Propiedad para almacenar la edad
+
+        // Método Clone() que crea una COPIA SUPERFICIAL del objeto Persona
+        public object Clone()
+        {
+            return this.MemberwiseClone(); // Clona el objeto sin duplicar referencias internas
+        }
+    }
+}
---
+++ PatronPrototype/Program.cs
@@ -0,0 +1,22 @@
+﻿// Importa el espacio de nombres PatronPrototype
+using PatronPrototype;
+using System.Text.Json; // Espacio de nombres para trabajar con serialización JSON
+using System.Text.Json.Nodes; // Trabaja con nodos JSON
+
+// Creación de un objeto Persona original
+var persona1 = new Persona { nombleCompleto = "Luis Angel Morel", edad = 20 };
+
+// Creación de una COPIA del objeto persona1 utilizando el método Clone()
+var persona2 = persona1.Clone() as Persona;
+
+// Se cambia el nombre y edad en la copia para verificar que afecta solo a la referencia compartida
+persona2.nombleCompleto = "Felix Morel";
+persona2.edad = 45;
+
+// Se serializan ambos objetos a formato JSON para mostrar los datos en consola
+var datos = JsonSerializer.Serialize(persona1);
+var datos1 = JsonSerializer.Serialize(persona2);
+
+// Muestra los objetos serializados en consola
+Console.WriteLine(datos);
+Console.WriteLine(datos1);
